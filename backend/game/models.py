ba8023c9c3daf955684b1a4334af37d6bbca882a
irakli <maniaika07@gmail.com>
2026-09-13 19:37:56 +0400

    feat(backend): add time controls and game results

--- backend/game/models.py
+++ backend/game/models.py
@@ -8,6 +8,16 @@ class Game(models.Model):
         ACTIVE = "active"
         FINISHED = "finished"
 
+    class TimeControl(models.IntegerChoices):
+        THREE = 3
+        FIVE = 5
+        TEN = 10
+
+    class Result(models.TextChoices):
+        WHITE_WINS = "white_wins"
+        BLACK_WINS = "black_wins"
+        DRAW = "draw"
+
     white_player = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -30,6 +40,26 @@ class Game(models.Model):
         max_length=10,
         choices=Status.choices,
         default=Status.WAITING,
+    )
+
+    result = models.CharField(
+        max_length=10,
+        choices=Result.choices,
+        null=True,
+        blank=True,
+    )
+
+    white_time = models.PositiveIntegerField()
+    black_time = models.PositiveIntegerField()
+
+    time_control = models.PositiveIntegerField(
+        choices=TimeControl.choices,
+        default=TimeControl.TEN,
+    )
+
+    turn_started_at = models.DateTimeField(
+        null=True,
+        blank=True,
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
